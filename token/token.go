8aa79b51d569c329328158447b43e4665793acaf
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-04-29 21:43:24 +0200

    Lex all the one-character tokens.

--- token/token.go
+++ token/token.go
@@ -16,8 +16,15 @@ const (
 	INT   = "INT"   // 123456 ...
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimiters
 	COMMA     = ","
