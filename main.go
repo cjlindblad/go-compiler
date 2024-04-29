aa35e0ec3a19312ea67e1567a5b4baf23af39323
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-04-29 22:17:25 +0200

    Lexer repl!!

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Println("Let's go!!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
