b2c347f43f058605062aaf9c7a57088c1c439b13
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-06-15 12:15:55 +0200

    Added test for closures.

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -195,6 +195,19 @@ func TestFunctionApplication(t *testing.T) {
 	}
 }
 
+func TestClosures(t *testing.T) {
+	input := `
+	let newAdder = fn(x) {
+	  fn(y) { x + y };
+	};
+
+	let addTwo = newAdder(2);
+	addTwo(2);
+	`
+
+	testIntegerObject(t, testEval(input), 4)
+}
+
 func TestErrorHandling(t *testing.T) {
 	tests := []struct {
 		input           string
