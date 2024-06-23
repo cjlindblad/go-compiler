c80a8b0a89d758ec329379bd05ad696e5ce9c4a0
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-06-23 21:12:07 +0200

    We can quote expressions.

--- object/object.go
+++ object/object.go
@@ -21,6 +21,7 @@ const (
 	BUILTIN_OBJ      = "BUILTIN"
 	ARRAY_OBJ        = "ARRAY"
 	HASH_OBJ         = "HASH"
+	QUOTE_OBJ        = "QUOTE"
 )
 
 type Object interface {
@@ -171,6 +172,15 @@ func (h *Hash) Inspect() string {
 	out.WriteString("}")
 
 	return out.String()
+}
+
+type Quote struct {
+	Node ast.Node
+}
+
+func (q *Quote) Type() ObjectType { return QUOTE_OBJ }
+func (q *Quote) Inspect() string {
+	return "QUOTE(" + q.Node.String() + ")"
 }
 
 type Error struct {
