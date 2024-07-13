664e1bdbd54f3b4565d51f73cfb9b03e1dba1381
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-07-13 11:01:46 +0200

    Our VM can add!

--- vm/vm.go
+++ vm/vm.go
@@ -45,6 +45,12 @@ func (vm *VM) push(o object.Object) error {
 	return nil
 }
 
+func (vm *VM) pop() object.Object {
+	o := vm.stack[vm.sp-1]
+	vm.sp--
+	return o
+}
+
 func (vm *VM) Run() error {
 	for ip := 0; ip < len(vm.instructions); ip++ {
 		op := code.Opcode(vm.instructions[ip])
@@ -58,6 +64,14 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpAdd:
+			right := vm.pop()
+			left := vm.pop()
+			leftValue := left.(*object.Integer).Value
+			rightValue := right.(*object.Integer).Value
+
+			result := leftValue + rightValue
+			vm.push(&object.Integer{Value: result})
 		}
 	}
 
