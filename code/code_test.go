9bd64390820f11f43c48aaf8713b29c3338acf0b
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-07-13 10:53:31 +0200

    We can format the OpAdd instruction as a string.

--- code/code_test.go
+++ code/code_test.go
@@ -31,14 +31,14 @@ func TestMake(t *testing.T) {
 
 func TestInstructionsString(t *testing.T) {
 	instructions := []Instructions{
-		Make(OpConstant, 1),
+		Make(OpAdd),
 		Make(OpConstant, 2),
 		Make(OpConstant, 65535),
 	}
 
-	expected := `0000 OpConstant 1
-0003 OpConstant 2
-0006 OpConstant 65535
+	expected := `0000 OpAdd
+0001 OpConstant 2
+0004 OpConstant 65535
 `
 
 	concatted := Instructions{}
@@ -47,7 +47,7 @@ func TestInstructionsString(t *testing.T) {
 	}
 
 	if concatted.String() != expected {
-		t.Errorf("instructions wrongly formatted.\nwant=%q\ngot=%q",
+		t.Errorf("instructions wrongly formatted.\nwant=%q\ngot =%q",
 			expected, concatted.String())
 	}
 }
