31156892962acfb8d329cf0c0b96a33337f976af
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-07-16 13:52:20 +0200

    Emit comparison operator opcodes.

--- code/code.go
+++ code/code.go
@@ -21,6 +21,10 @@ const (
 
 	OpTrue
 	OpFalse
+
+	OpEqual
+	OpNotEqual
+	OpGreaterThan
 )
 
 type Definition struct {
@@ -39,6 +43,10 @@ var definitions = map[Opcode]*Definition{
 
 	OpTrue:  {"OpTrue", []int{}},
 	OpFalse: {"OpFalse", []int{}},
+
+	OpEqual:       {"OpEqual", []int{}},
+	OpNotEqual:    {"OpNotEqual", []int{}},
+	OpGreaterThan: {"OpGreaterThan", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
