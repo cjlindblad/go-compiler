842b92502cf0f6dc0c90eac5f94753ee47870720
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-07-21 21:45:38 +0200

    Compiler handles index expressions.

--- code/code.go
+++ code/code.go
@@ -39,6 +39,7 @@ const (
 
 	OpArray
 	OpHash
+	OpIndex
 )
 
 type Definition struct {
@@ -75,6 +76,7 @@ var definitions = map[Opcode]*Definition{
 
 	OpArray: {"OpArray", []int{2}},
 	OpHash:  {"OpHash", []int{2}},
+	OpIndex: {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
