f4a52621873b5a19a0f050a42fcf245853b1e1b5
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-07-23 22:06:59 +0200

    Compile functions without bodies.

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -285,6 +285,9 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if c.lastInstructionIs(code.OpPop) {
 			c.replaceLastPopWithReturn()
 		}
+		if !c.lastInstructionIs(code.OpReturnValue) {
+			c.emit(code.OpReturn)
+		}
 
 		instructions := c.leaveScope()
 
