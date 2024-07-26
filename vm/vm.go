952396e6d0174f9d66053f4ffaceee2e20287a25
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-07-26 22:06:59 +0200

    Handle wrong number of arguments to function call.

--- vm/vm.go
+++ vm/vm.go
@@ -278,6 +278,11 @@ func (vm *VM) callFunction(numArgs int) error {
 		return fmt.Errorf("calling non-function")
 	}
 
+	if numArgs != fn.NumParameters {
+		return fmt.Errorf("wrong number of arguments: want=%d, got=%d",
+			fn.NumParameters, numArgs)
+	}
+
 	frame := NewFrame(fn, vm.sp-numArgs)
 	vm.pushFrame(frame)
 
